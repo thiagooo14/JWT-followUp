ee4aeb1c5a5bef5263531f72cc465ba73d3e9d40
Thiago B. S. Tavares <thiagooo15@gmail.com>
2023-04-03 15:30:33 -0300

    projeto com as alterações do ale

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,50 +1,56 @@
 import jwtDecode from 'jwt-decode';
 import { useEffect, useState } from 'react';
 import TextComponent from '../components/TextComponent';
-
-// const tolken =
-//   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwidXNlcm5hbWUiOiJKb2huIERvZSIsImlhdCI6MTUxNjIzOTAyMn0.p5Csu2THYW5zJys2CWdbGM8GaWjpY6lOQpdLoP4D7V4';
-
+import './style.css'
 
 const Main = () => {
-  const [batata, setBatata] = useState('');
   const [token, setToken] = useState('');
-  let decode;
+  const [decode, setDecode] = useState();
+  const [string, setString] = useState('');
+  const [show, setShow] = useState('')
 
-  useEffect(() => {
-    // setName(jwtDecode(tolken).username);
-    setBatata(JSON.stringify(decode));
-  }, [decode]);
-
-  
-  
-  // const formatObject = (obj) => {
-    //   return obj && obj.split(',')
-    // }
-    
-    // console.log(formatObject(decode))
-    
-    try {
-      decode = jwtDecode(token)
-    } catch (error) {
-      // console.error(error)
-    }
-  console.log(decode)
-    
   const changeName = (token) => {
     setToken(token)
   }
 
+  useEffect(() => {
+    try {
+      setDecode(jwtDecode(token))
+      setShow('visible')
+    } catch (error) {
+      setString('Token Invalido')
+      setShow('none')
+    }
+  }, [token]);
 
+  useEffect(() => {
+    setString(decode);
+  }, [decode]);
+  
+  const formatObject = () => {
+    if (string) {
+      return string.split(/[,{}]/)
+    }
+  }
 
   return (
     <>
+      <h1>Token:</h1>
       <TextComponent changeName={changeName} />
-      <h2>token:</h2>
-      <p>{batata}</p>
-      {/* <p>{formatObject(batata)}</p> */}
-      {/* <p>{formatObject(batata)[1]}</p>
-      <p>{formatObject(batata)[2]}</p> */}
+      <h2>Decode:</h2>
+      <pre className='show-code'>
+        {/* {JSON.stringify(string, null, 4)} */}
+        {string.name}
+      {/* {string && (
+        <>
+          <p className={show}>{ '{' }</p>
+          {formatObject().map((e, i) => (
+            <p key={i} className="text-code">{e}</p>
+          ))}
+          <p className={show}>{'}'}</p>
+        </>
+        )} */}
+      </pre>
     </>
   );
 }
